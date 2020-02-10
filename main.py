c978dddda31ab7446d0cc8e51ddcae60b4421d76
royassis <royassisg@gmail.com>
2020-02-10 09:42:45 +0200

    cmnt

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pandas as pd
 path = r"C:\Users\roy\Desktop\sensors.xlsx"
 out_path = r"C:\Users\roy\Desktop\sensorsOUT.xlsx"
 
-df = pd.read_excel(path, sheet_name='working copy', index_col='id')
+df = pd.read_excel(path, sheet_name='working copy',  index_col='id')
 original = df.copy()
 
 #################
@@ -15,12 +15,12 @@ original = df.copy()
 
 # Condition 1 - Sensor tagname must adhere to the following regex
 regex = r'^[A-z][A-z]\d\d[A-z][A-z]\d\d.*$'
-cond1 = ~df.sensor_tagname.dropna().str.match(regex, na= False)
+cond1 = ~ df.sensor_tagname.dropna().str.match(regex, na= False)
 
 # Condition 2 - Same כלי with multiple same name sensors
-cond2 = df.duplicated(['op_area','sensor_kind','device_number'], keep = False)
+cond2 = df.duplicated(['op_area','sensor_kind','device_kind','device_number'], keep = False)
 
-# Condition 3 - Suffixes that show only once
+# Condition 3 - Suffixes that show only for one device
 result = df.groupby(["sensor_suffix"])["sensor_suffix"].count()
 suspicious_suffixes = result[result == 1].index
 cond3= df["sensor_suffix"].isin(suspicious_suffixes)
@@ -36,8 +36,4 @@ df["nogood"][conds] = 1
 #################
 # Output to file
 #################
-writer = pd.ExcelWriter(out_path, engine = 'xlsxwriter')
-original.to_excel(writer, sheet_name = 'sheet1', index_label= 'id')
-df.sort_index().to_excel(writer, sheet_name = 'sheet2', index_label= 'id')
-writer.save()
-writer.close()
+df.sort_index().to_excel(out_path, index_label= 'id')
