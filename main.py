0edff3a43b172d6a6ac6769b22384febb5b7f771
royassis <royassisg@gmail.com>
2020-02-10 13:31:36 +0200

    cmnt

--- main.py
+++ main.py
@@ -7,23 +7,14 @@ from functions import get_col_widths
 path = r"C:\Users\roy\Desktop\sensors.xlsx"
 out_path = r"C:\Users\roy\Desktop\sensorsOUT.xlsx"
 
-cols = ['id','lVarId', 'sensor_tagname', 'device', 'device_kind', 'device_number',
-       'op_area','HMI', 'description','prod_line','sensor_kind', 'sensor_suffix']
+cols = ['id','lVarId', 'sensor_tagname',
+       'HMI', 'description','prod_line','sensor_kind']
 df = pd.read_excel(path, sheet_name='working copy', usecols = cols)
 
+
 #################
-# Relevent sensors
+# Breakdown with regex
 #################
-op_area_values = ['CC01','CC02','CC03','CC04']
-device_kind_values = ['CD','CR','CV','WC','WD']
-cond =  (df['op_area'].isin(op_area_values)) & (df['device_kind'].isin(device_kind_values)) | \
-         (df['sensor_tagname'].str.contains(r'MR02', na= False) ) | \
-         (df['sensor_tagname'].str.contains(r'MR04+TK', na= False) ) | \
-         (df['sensor_tagname'].str.contains(r'PS01MP', na=False))
-
-
-df = df[cond].sort_values(['HMI','op_area','device_kind','device_number','sensor_suffix'])
-
 breakdown = df.sensor_tagname.str.extract(r"(?P<prefix>^.*?)"
                                              r"(?P<op_area_name>[A-z][A-z])"
                                              r"(?P<op_area_number>\d\d)"
@@ -35,6 +26,21 @@ breakdown = df.sensor_tagname.str.extract(r"(?P<prefix>^.*?)"
 df = df.merge(breakdown, right_index= True,  left_index= True)
 
 #################
+# Relevent sensors
+#################
+op_area_values = ['CC01','CC02','CC03','CC04']
+device_kind_values = ['cd','cr','cv','wc','wd']
+cond =  (df['op_area_name'].isin(op_area_values)) & (df['device_kind'].isin(device_kind_values)) | \
+         (df['sensor_tagname'].str.contains(r'MR02', na= False) ) | \
+         (df['sensor_tagname'].str.contains(r'MR04+TK', na= False) ) | \
+         (df['sensor_tagname'].str.contains(r'PS01MP', na=False))
+
+
+
+df = df[cond].sort_values(['HMI','op_area_name','device_kind','device_number','suffix'])
+
+
+#################
 # Is ok Conditions
 #################
 
@@ -43,12 +49,12 @@ regex = r'^[A-z][A-z]\d\d[A-z][A-z]\d\d.*$'
 cond1 = ~ df.sensor_tagname.dropna().str.match(regex, na= False)
 
 # Condition 2 - Same כלי with multiple same name sensors
-cond2 = df.duplicated(['op_area','sensor_kind','device_kind','device_number'], keep = False)
+cond2 = df.duplicated(['op_area_name','sensor_kind','device_kind','device_number'], keep = False)
 
 # Condition 3 - Suffixes that show only for one device
-result = df.groupby(["sensor_suffix"])["sensor_suffix"].count()
+result = df.groupby(["suffix"])["suffix"].count()
 suspicious_suffixes = result[result == 1].index
-cond3= df["sensor_suffix"].isin(suspicious_suffixes)
+cond3= df["suffix"].isin(suspicious_suffixes)
 
 conds = cond1 | cond2 | cond3
 
@@ -78,7 +84,7 @@ worksheet = writer.sheets['Sheet1']
 format1 = workbook.add_format({'bg_color': '#FFC7CE',
                                'font_color': '#9C0006'})
 
-range = 'M2:P3000'
+range = 'O2:R3000'
 worksheet.conditional_format(range, {'type': 'cell',
                                          'criteria': '=',
                                          'value': 1,
