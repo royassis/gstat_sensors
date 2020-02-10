f7f088fb48b6a9c660d91f3c4015a532fb65455c
royassis <royassisg@gmail.com>
2020-02-10 08:39:02 +0200

    multiple excel sheets

--- main.py
+++ main.py
@@ -7,31 +7,26 @@ path = r"C:\Users\roy\Desktop\sensors.xlsx"
 out_path = r"C:\Users\roy\Desktop\sensorsOUT.xlsx"
 
 df = pd.read_excel(path, sheet_name='working copy', index_col='id')
+original = df.copy()
 
 #################
 # Conditions
 #################
 
 # Condition 1 - Sensor tagname must adhere to the following regex
-cond1 = ~df.sensor_tagname.dropna().str.match(r'^[A-z][A-z]\d\d[A-z][A-z]\d\d.*$', na= False)
+regex = r'^[A-z][A-z]\d\d[A-z][A-z]\d\d.*$'
+cond1 = ~df.sensor_tagname.dropna().str.match(regex, na= False)
 
 # Condition 2 - Same כלי with multiple same name sensors
-cond2 = df.duplicated(['sensor_kind','מספר כלי'])
+cond2 = df.duplicated(['op_area','sensor_kind','device_number'], keep = False)
 
-# Condition 3 - Suffix that show only once in אזור
-some_other_stuff = df.groupby(['sensor_suffix','אזור'])['sensor_suffix'].count()
-lonly_index = some_other_stuff[some_other_stuff == 1].index
-a,b = lonly_index.get_level_values(0), lonly_index.get_level_values(1)
-cond3 = ((df['sensor_suffix'] == 'a') & (df['אזור'] == 'b'))
-
-pvt = df.pivot_table(values ='sensor_tagname' , index = ['סוג כלי'], columns ='sensor_suffix' , aggfunc = 'count', fill_value= 0)
-unstacked = pvt.unstack().reset_index().rename({0:"count_all"},axis = 1)
-only_one = unstacked[unstacked.count_all == 1]
-
-## Self-Join suffixe/אזור/מספר כלי on  אזור,suffix
-## suffixe == suffixe and אזור == אזור and מספר כלי != מספר כלי
+# Condition 3 - Suffixes that show only once
+result = df.groupby(["sensor_suffix"])["sensor_suffix"].count()
+suspicious_suffixes = result[result == 1].index
+cond3= df["sensor_suffix"].isin(suspicious_suffixes)
 
 conds = cond1 | cond2 | cond3
+
 #################
 # Apply masks
 #################
@@ -41,4 +36,8 @@ df["nogood"][conds] = 1
 #################
 # Output to file
 #################
-df.sort_index().to_excel(out_path, index_label= 'id')
+writer = pd.ExcelWriter(out_path, engine = 'xlsxwriter')
+original.to_excel(writer, sheet_name = 'sheet1', index_label= 'id')
+df.sort_index().to_excel(writer, sheet_name = 'sheet2', index_label= 'id')
+writer.save()
+writer.close()
