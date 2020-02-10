13472e7c27c5fdb3154860e0f742d0e063b4f0fc
royassis <royassisg@gmail.com>
2020-02-10 11:45:55 +0200

    worksheet formatting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from . import functions
 
 #################
 # Import data
@@ -10,10 +11,17 @@ cols = ['id','lVarId', 'sensor_tagname', 'device', 'device_kind', 'device_number
        'op_area','HMI', 'description','prod_line','sensor_kind', 'sensor_suffix']
 df = pd.read_excel(path, sheet_name='working copy', usecols = cols ,index_col='id')
 
-prod_line_values = ['CC01','CC02','CC03','CC04']
-cond0 = (df['prod_line'].isin(prod_line_values)) & ()
 #################
-# Conditions
+# Relevent sensors conditions
+#################
+op_area_values = ['CC01','CC02','CC03','CC04']
+device_kind_values = ['CD','CR','CV','WC','WD']
+cond0 = (df['op_area'].isin(op_area_values)) & (df['device_kind'].isin(device_kind_values))
+
+df = df[cond0].sort_values(['op_area','device_kind','device_number','sensor_suffix'])
+
+#################
+# Is ok Conditions
 #################
 
 # Condition 1 - Sensor tagname must adhere to the following regex
@@ -33,10 +41,37 @@ conds = cond1 | cond2 | cond3
 #################
 # Apply masks
 #################
-df["nogood"] = 0
-df["nogood"][conds] = 1
+df["bad regex"] = 0
+df["redundent suffix"] = 0
+df["lonly suffix"] = 0
+
+df["bad regex"][cond1] = 1
+df["redundent suffix"][cond2] = 1
+df["lonly suffix"][cond3] = 1
+
 
 #################
 # Output to file
 #################
-df.sort_index().to_excel(out_path, index_label= 'id')
+writer = pd.ExcelWriter(out_path, engine='xlsxwriter')
+df.to_excel(writer, index_label= 'id', sheet_name='Sheet1')
+
+workbook  = writer.book
+worksheet = writer.sheets['Sheet1']
+
+format1 = workbook.add_format({'bg_color': '#FFC7CE',
+                               'font_color': '#9C0006'})
+
+range = 'M2:O3000'
+worksheet.conditional_format(range, {'type': 'cell',
+                                         'criteria': '=',
+                                         'value': 1,
+                                         'format': format1})
+
+worksheet.freeze_panes(1, 0)
+
+
+for i, width in enumerate(functions.get_col_widths(df)):
+    worksheet.set_column(i, i, width)
+
+writer.save()
