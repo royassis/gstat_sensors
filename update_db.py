35fd3e8ff35ead68775307774c315f905ae314c8
User <royassisg@gmail.com>
2020-02-19 10:44:42 +0200

    logging

--- update_db.py
+++ update_db.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 db_name = 'sensors'
-
+log_tbl = 'logs'
 l = [
     ['lines',r'C:\Users\User\PycharmProjects\gstat_sensors\data\lines.xlsx'],
     ['sensor_data', r'C:\Users\User\PycharmProjects\gstat_sensors\data\sensors_base.xlsx'],
@@ -15,6 +15,9 @@ connstr = 'mssql+pyodbc://DESKTOP-TL42QJI\SQLEXPRESS/{}?driver=SQL+Server+Native
 engine = create_engine(connstr)
 
 for d in l:
+    log_data = pd.DataFrame([[pd.Timestamp.now(), d[0]]], columns = ['logtime','tablename'])
+    log_data.to_sql(log_tbl, con=engine, if_exists= 'append')
+
     df = pd.read_excel(d[1])
-    df.to_sql(d[0], con=engine, if_exists= 'replace')
+    df.to_sql(d[0], con=engine, if_exists= 'replace', index_label= 'id', index= False)
 
