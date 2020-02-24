91f446c1f776c9487343aac66b530cd7689b9cc1
User <royassisg@gmail.com>
2020-02-24 16:19:54 +0200

    schedualer

--- update_db.py
+++ update_db.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sqlalchemy import create_engine
+from datetime import datetime
 
 
 
@@ -18,7 +19,11 @@ def main ():
     connstr = 'mssql+pyodbc://DESKTOP-TL42QJI\SQLEXPRESS/{}?driver=SQL+Server+Native+Client+11.0'.format(db_name)
     engine = create_engine(connstr)
 
+    date = datetime.now().strftime("%Y-%m-%d")
+
     for tbl,path in l:
+        log_data = pd.DataFrame([[date, tbl]], columns = ['logtime','tablename'])
+        log_data.to_sql(log_tbl, con=engine, if_exists= 'append', index= False)
         df = pd.read_excel(path)
         df.to_sql(tbl, con=engine, if_exists= 'replace', index_label= 'id', index= False)
 
