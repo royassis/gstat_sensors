e6fba069d98c319db408c6fb2ca159b472b7b659
royassis <royassisg@gmail.com>
2020-02-26 17:14:13 +0200

    add the lines to the joined file

--- add_the_lines.py
+++ add_the_lines.py
@@ -12,7 +12,8 @@ filename = r'tables_cottage_stages.csv'
 fullpath = base+filename
 b = pd.read_csv(fullpath)\
     .rename({'id':'read_id','batchid':'batch_id'},axis = 1)\
-    .astype({"device_number":str})
+    .dropna(axis =0, how = 'any', subset = ['device_number'])\
+    .astype({'device_number':int})
 b['device_kind'] = b['device_kind'].str.lower()
 
 a.merge(b, left_on =['device_kind','device_number'],right_on =['device_kind','device_number'])
