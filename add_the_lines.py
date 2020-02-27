e56f24836c09f9304fd1f0b34587c3b3cb8863b9
User <royassisg@gmail.com>
2020-02-27 10:07:33 +0200

    read files and manipulate

--- add_the_lines.py
+++ add_the_lines.py
@@ -1,19 +1,58 @@
 import pandas as pd
+import numpy as np
 from functions import lower_all_strings
 
 base = r'data/'
 
-filename = r'sensor_joined.csv'
+# ------------------------------------ read file 1 ------------------------------------  #
+filename = r'sensor_joined.xlsx'
 fullpath = base+filename
-a = pd.read_csv(fullpath)\
+
+dtypes = {'sensor_id' : np.int32,
+ 'lVarId' : np.int32,
+ 'HMI' : np.int32,
+ 'op_area_name' : str,
+ 'op_area_number' : np.int32,
+ 'device_kind' : str,
+ 'device_number' : np.int32,
+ 'sensor_tagname' : str,
+ 'sensor_name' : str,
+ 'sensor_name_edited' : str,
+ 'desc' : str,
+ 'out_pipes' : str,
+ 'in_pipes' : str}
+
+a = pd.read_excel(io = fullpath,
+                  dtypes = dtypes)\
     .rename({'id':'sensor_id'},axis = 1)
 
+a['device_number'] = pd.to_numeric(a['device_number'], errors = 'coerce')
+
+a = a.dropna(axis = 0 , subset = ['device_number']).astype({'device_number':np.int32})
+
+# ------------------------------------ read file 2 ------------------------------------  #
 filename = r'tables_cottage_stages.csv'
 fullpath = base+filename
-b = pd.read_csv(fullpath)\
-    .rename({'id':'read_id','batchid':'batch_id'},axis = 1)\
-    .dropna(axis =0, how = 'any', subset = ['device_number'])\
-    .astype({'device_number':int})
+
+dtypes = {'batchid' : int,
+         'stageid' : int,
+         'stage' : str}
+date_cols = ['start', 'finish']
+
+b = pd.read_csv(filepath_or_buffer = fullpath,
+                parse_dates = date_cols)\
+    .rename({'stageid':'device_number',
+             'stage': 'device_kind',
+             'id':'read_id',
+             'batchid':'batch_id'} ,axis = 1)
+
+b['device_number'] = pd.to_numeric(b['device_number'], errors = 'coerce')
+
+b = b.dropna(axis = 0 , subset = ['device_number']).astype({'device_number':np.int32})
+
 b['device_kind'] = b['device_kind'].str.lower()
 
+# ------------------------------------ merge files ------------------------------------  #
 a.merge(b, left_on =['device_kind','device_number'],right_on =['device_kind','device_number'])
+
+
