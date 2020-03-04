e438f02080982c58828c711d3f915a8557460ff5
User <royassisg@gmail.com>
2020-03-04 09:29:14 +0200

    minor fixes

--- add_the_lines_new.py
+++ add_the_lines_new.py
@@ -2,27 +2,28 @@
 import pandas as pd
 import numpy as np
 import os
+
 pd.options.display.width = 0
 
 # Set base dirs
-mapper_base = r'resources/'
-base = r'data/'
+mapper_base = r'resources'
+base = r'data'
 
 # ------------------------------------ read time_mapper ------------------------------------  #
 filename = r'time_mapper.csv'
-fullpath = mapper_base + filename
+fullpath = os.path.join(mapper_base, filename)
 time_mapper = pd.read_csv(fullpath)
 
 # ------------------------------------ read line_mapper ------------------------------------  #
 filename = r'line_mapper.csv'
-fullpath = mapper_base + filename
+fullpath = os.path.join(mapper_base, filename)
 line_mapper = pd.read_csv(fullpath)
 line_mapper = line_mapper.assign(device_number=line_mapper['device_number'].str.split(','))
 
 # ------------------------------------ read and format file 1 ------------------------------------  #
 # Read data
 filename = r'sensor_joined.xlsx'
-fullpath = base + filename
+fullpath = os.path.join(base, filename)
 
 dtypes = {'sensor_id': np.int32,
           'lVarId': np.int32,
@@ -46,13 +47,13 @@ a = pd.read_excel(io=fullpath,
 a['device_number'] = pd.to_numeric(a['device_number'], errors='coerce')
 
 # Concat xx(device_kind) to op_area_name and number
-cond = a['device_kind']=='xx'
-a['device_kind'][cond] = a['op_area_name'][cond]+a['op_area_number'][cond].astype(str)+'xx'
+cond = a['device_kind'] == 'xx'
+a.loc[cond, 'device_kind'] = a['op_area_name'][cond] + a['op_area_number'][cond].astype(str) + 'xx'
 
 # ------------------------------------ read and format file 2 ------------------------------------  #
 # Read data
 filename = r'tables_cottage_stages.csv'
-fullpath = base + filename
+fullpath = os.path.join(base, filename)
 
 dtypes = {'batchid': int,
           'stageid': int,
@@ -81,7 +82,7 @@ b = b.assign(delta_q_bins=b.groupby(['device_kind'])['delta'].transform(
 # Remove 'packing' category
 # Set some dates to Null
 cond1 = (b['device_kind'] != 'packing')
-b = b[cond1]
+b.loc[:, :] = b[cond1]
 
 cond2 = (b['start'].dt.year != 2019) \
         | (b['finish'].dt.year != 2019) \
@@ -90,7 +91,7 @@ cond2 = (b['start'].dt.year != 2019) \
         | (b['delta'] < 0) \
         | (b['delta'] > 15)
 
-b[cond2]['finish', 'start'] = None
+b.loc[cond2, ['finish', 'start']] = None
 
 # Groupby in order to remove batches that have multiple device kinds
 b = b.groupby(['batch_id', 'device_kind']).agg({'device_number': 'max',
@@ -108,11 +109,11 @@ xxx = b.pivot_table(index='batch_id',
                     aggfunc='first')
 
 # Flatten column multiindex
-xxx.columns = ['_'.join(col).strip() for col in xxx.columns.values]
+xxx.columns = ['_'.join(col).strip() for col in xxx.columns]
 
 # Do the same for the pipes
 # make an empty pipes df and concat with previous df
-pipes = ['cc1xx','ps1xx']
+pipes = ['cc1xx', 'ps1xx']
 pipes = ['start_' + i for i in pipes] + ['finish_' + i for i in pipes] + ['device_number_' + i for i in pipes]
 
 pipes = pd.DataFrame(columns=pipes,
@@ -126,7 +127,6 @@ xxx = pd.concat([xxx, pipes], axis=1).reset_index()
 for _, r in time_mapper.iterrows():
     xxx[r['to']] = xxx[r['from']]
 
-
 # Set line numbers for each pipe by the logic in the 'line_mapper' file
 for _, r in line_mapper.iterrows():
     line_list = r['device_number']
@@ -135,8 +135,7 @@ for _, r in line_mapper.iterrows():
     line_column = r['col_b']
 
     cond = xxx[r['col_a']].isin(line_list)
-    xxx[line_column][cond] = r['line_number']
-
+    xxx.loc[cond, line_column] = r['line_number']
 
 # Convert back to the long form
 stubnames = ["device_number", "start", "finish"]
@@ -146,9 +145,12 @@ xxx = pd.wide_to_long(df=xxx, stubnames=stubnames, i="batch_id", j="device_kind"
 merged = a.merge(xxx, left_on=['device_kind', 'device_number'], right_on=['device_kind', 'device_number'])
 merged = merged.sort_values(['batch_id', 'device_kind'], ascending=[True, True]).reset_index()
 
+filename = r'batches_out.xlsx'
+fullpath = os.path.join(base, filename)
+merged.to_excel(fullpath)
 
-# ------------------------------------ find sensors that did not match with batch ------------------------------------  #
-m2 = a.merge(xxx,how='left', left_on=['device_kind', 'device_number'], right_on=['device_kind', 'device_number'])
+# ----------------------------------- find sensors that did not match with batch -----------------------------------  #
+m2 = a.merge(xxx, how='left', left_on=['device_kind', 'device_number'], right_on=['device_kind', 'device_number'])
 cond = m2['batch_id'].isna()
 
-m2[cond].to_clipboard()
+# m2[cond].to_clipboard()
